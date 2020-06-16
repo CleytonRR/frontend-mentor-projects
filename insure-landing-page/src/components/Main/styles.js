af24ea38c6847d9d5ee32cf449adb5020ee24ff8
cleytonRR <cleytonrodrigues69@live.com>
2020-06-16 20:31:45 -0300

    Added reponsive for Main component

--- insure-landing-page/src/components/Main/styles.js
+++ insure-landing-page/src/components/Main/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
-import BackgroundSvg from '../../assets/bg-pattern-intro-right-desktop.svg';
+import BackgroundSvg from '../../assets/bg-pattern-how-we-work-desktop.svg';
+import BackgroundSvgMobile from '../../assets/bg-pattern-how-we-work-mobile.svg';
 
 export const Container = styled.section`
   width: 100vw;
@@ -22,8 +23,19 @@ export const Title = styled.h2`
     display: block;
     width: 100px;
     height: 1px;
-    margin-bottom: 50px;
+    margin-bottom: 20px;
     background-color: hsl(256, 26%, 20%);
+  }
+
+  @media (max-width: 770px) {
+    font-size: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media (max-width: 438px) {
+    font-size: 35px;
   }
 `;
 
@@ -31,9 +43,22 @@ export const ContainerItems = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: 890px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
-export const Item = styled.div``;
+export const Item = styled.div`
+  @media (max-width: 890px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
 
 export const IconItem = styled.img``;
 
@@ -53,21 +78,30 @@ export const DescriptionItem = styled.p`
 export const ContainerAbout = styled.div`
   background-color: hsl(256, 26%, 20%);
   background-image: url(${BackgroundSvg});
-  background-position: right;
+  background-position: right top;
   background-repeat: no-repeat;
-  background-size: 500px 500px;
   width: 100%;
-  height: 200px;
   margin-top: 100px;
   display: flex;
   justify-content: space-around;
   align-items: center;
+
+  @media (max-width: 770px) {
+    background-image: url(${BackgroundSvgMobile});
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const AboutTitle = styled.h5`
   font-family: 'DM Serif Display', serif;
   font-size: 45px;
   color: hsl(0, 0%, 98%);
+
+  @media (max-width: 770px) {
+    text-align: center;
+  }
 `;
 
 export const AboutButton = styled.button`
@@ -84,5 +118,9 @@ export const AboutButton = styled.button`
   &:hover {
     background-color: hsl(0, 0%, 98%);
     color: hsl(256, 26%, 20%);
+  }
+
+  @media (max-width: 770px) {
+    margin-bottom: 50px;
   }
 `;
