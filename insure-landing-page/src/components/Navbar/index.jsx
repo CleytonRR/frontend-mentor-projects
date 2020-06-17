866b35826c6d4853432d1dceca2d2909c579325d
cleytonRR <cleytonrodrigues69@live.com>
2020-06-16 23:57:05 -0300

    Created reponsive menu

--- insure-landing-page/src/components/Navbar/index.jsx
+++ insure-landing-page/src/components/Navbar/index.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { ContainerMenu, Menu, LogoMenu, MenuItem, ButtonMenu } from "./style";
+import { ContainerMenu, Menu, LogoMenu, Hamburger, MenuItem, ButtonMenu } from "./style";
 
 import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(true)
     return (
         <ContainerMenu>
             <LogoMenu src={logo} />
-            <Menu>
+            <Menu openMenu={open}>
                 <MenuItem>How we work</MenuItem>
                 <MenuItem>Blog</MenuItem>
                 <MenuItem>Account</MenuItem>
                 <ButtonMenu>View Plans</ButtonMenu>
             </Menu>
+
+            <Hamburger onClick={() => setOpen(!open)} openMenu={open} />
         </ContainerMenu>
     )
 }
